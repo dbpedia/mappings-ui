f172d5a6cb072d1c33575472ef8dc2b7918fe246
ismaro3 <ismaro.394@gmail.com>
2017-07-02 16:37:10 +0100

    Fixed .netrc in heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 const Composer = require('./index');
 const PeriodicOntologyService = require('./server/ontologyExport/periodicOntologyUpdateWorker');
+const GithubNetrc = require('./github-netrc');
 
 Composer((err, server) => {
 
@@ -12,5 +13,6 @@ Composer((err, server) => {
 
         console.log('Started the plot device on port ' + server.info.port);
         PeriodicOntologyService.start();
+        GithubNetrc.putLoginIntoNetrc();
     });
 });
